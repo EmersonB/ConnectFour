13b185d1c8e102091a0dd04add19f3eca6e23089
EmersonB <eberlik@gmail.com>
2017-02-09 15:23:54 -0500

    Hehe

--- connect4.py
+++ connect4.py
@@ -1,6 +1,12 @@
 board = ["."] * 42
 player = 'x'
 
+def game_over(board):
+    for move in range(0,42):
+        if board[move] is not ".":
+            if(terminal_state(move) is not None):
+                return terminal_state(move)
+    return None
 
 def terminal_state(start_move):
     dirs = [1, -1, -7, 7, -8, 8, 6, -6]
@@ -17,14 +23,7 @@ def terminal_state(start_move):
 
 
 def is_legal(move):
-    if move < 0 or move > 41:
-        return False
-    if board[move] != -1:
-        return False
-    if move <= 34:
-        if board[move + 7] == -1:
-            return False
-    if move % 7 == 0:  # border
+    if board[move] is not '.':
         return False
     return True
 
@@ -34,6 +33,8 @@ def legal_moves():
 
 
 def make_move(move, player):
+    if not is_legal(move):
+        return False
     column = move
     while (board[column + 7] == "."):
         column += 7
@@ -55,6 +56,49 @@ def display():
         print(board[x], end="")
         if (x + 1) % 7 == 0:
             print(" ")
+    print('')
+
+def minimax(player, maxDepth, currentDepth):
+    if player == "x":
+        return max_dfs(board, player, maxDepth, currentDepth)[1]
+    else:
+        return min_dfs(board, player, maxDepth, currentDepth)[1]
+
+def max_dfs(board, player, maxDepth, currentDepth):
+    if(game_over(board)!= -1):
+        if(game_over(board)=="x"):
+            return 1000, None
+        if (game_over(board) == "o"):
+            return -1000, None
+        if(game_over(board)=="Draw"):
+            return 0, None
+    v = -1000
+    move = 0
+    for m in legal_moves():
+        tempBoard = make_move(m, player)
+        new_value = min_dfs(make_move( m, player), opponent(player), maxDepth, currentDepth + 1)[0]
+        if new_value > v:
+            v = new_value
+            move = m
+    return v, move
+
+def min_dfs(board, player, maxDepth, currentDepth):
+    if(game_over(board)!= -1):
+        if(game_over(board)=="x"):
+            return 1000, None
+        if (game_over(board) == "o"):
+            return -1000, None
+        if(game_over(board)=="Draw"):
+            return 0, None
+    v = 1000
+    move = 0
+    for m in legal_moves():
+        tempBoard = make_move(m, player)
+        new_value = max_dfs(make_move(m, player), opponent(player), maxDepth, currentDepth + 1)[0]
+        if new_value < v:
+            v = new_value
+            move = m
+    return v, move
 
 
 player = 'x'
@@ -62,9 +106,15 @@ display()
 move = int(input("move:"))
 move = make_move(move, player)
 display()
-while terminal_state(move) is None:
-    print(move)
+while game_over(board) is None:
+#while terminal_state(move) is None:
+    #print(move)
     player = opponent(player)
-    move = int(input("move:"))
-    move = make_move(move,player)
+    #move = int(input("move:"))
+    move = minimax(player,3,0)
+    #while make_move(move,player) is False:
+    #    move = int(input("move invalid:"))
+
     display()
+    print('')
+print(terminal_state(move)+' wins')
